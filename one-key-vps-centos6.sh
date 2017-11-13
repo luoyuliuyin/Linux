7c5042765bd6281bfbfbcd04b3142f6691a49a25
luoyuliuyin <luoyuliuyin@gmail.com>
2017-11-13 17:20:39 +0800

    Update one-key-vps-centos6.sh

--- one-key-vps-centos6.sh
+++ one-key-vps-centos6.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #####---shadowsocks---#####;
-yum install epel-release -y;yum update -y;yum install python-setuptools -y;easy_install pip;pip install shadowsocks;echo 'alias sslog="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '/usr/bin/ssserver -s ::0 -p 444 -k pengfeixiong -m aes-256-cfb --user nobody --workers 2 -d start' >> /etc/rc.local;
+yum install epel-release -y;yum update -y;yum install python-setuptools -y;easy_install pip;pip install shadowsocks;echo 'alias sslog="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '/usr/bin/ssserver -s ::0 -p 444 -k pengfeixiong -m aes-256-cfb --user nobody --workers 5 -d start' >> /etc/rc.local;
 
 #####---net_speeder---#####;
 yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '/opt/net_speeder/net_speeder eth0 "ip"' >> /etc/rc.local;
