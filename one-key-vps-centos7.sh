f230bdd93550b9182e0587d491592166c919fa46
luoyuliuyin <luoyuliuyin@gmail.com>
2017-11-13 12:02:06 +0800

    Update one-key-vps-centos7.sh

--- one-key-vps-centos7.sh
+++ one-key-vps-centos7.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #####---shadowsocks---#####;
-yum install epel-release -y;yum update -y;yum install python-setuptools -y;easy_install pip;pip install shadowsocks;echo 'alias sslog="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '[Unit]\nDescription=Shadowsocks\n\n[Service]\nType=forking\nExecStart=/usr/bin/ssserver -s ::0 -p 444 -k pengfeixiong -m aes-256-cfb --user nobody --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/shadowsocks.service;systemctl enable shadowsocks;
+yum install epel-release -y;yum update -y;yum install vim wget net-tools python-setuptools -y;easy_install pip;pip install shadowsocks;echo 'alias sslog="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '[Unit]\nDescription=Shadowsocks\n\n[Service]\nType=forking\nExecStart=/usr/bin/ssserver -s ::0 -p 444 -k pengfeixiong -m aes-256-cfb --user nobody --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/shadowsocks.service;systemctl enable shadowsocks;
 
 #####---net_speeder---#####;
-yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '[Unit]\nDescription=net_speeder\nAfter=network.service\n\n[Service]\nType=simple\nExecStart=/opt/net_speeder/net_speeder venet0 "ip"\n\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/net_speeder.service;systemctl enable net_speeder;
+yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '[Unit]\nDescription=net_speeder\nAfter=network.service\n\n[Service]\nType=simple\nExecStart=/opt/net_speeder/net_speeder eth0 "ip"\n\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/net_speeder.service;systemctl enable net_speeder;
 
 #####—ttf---#####;
 wget -P /usr/share/fonts/yahei https://raw.githubusercontent.com/yakumioto/YaHei-Consolas-Hybrid-1.12/master/YaHei%20Consolas%20Hybrid%201.12.ttf;
