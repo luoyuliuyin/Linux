a65c045841afbb939bc87036f86edbc843f73f70
luoyuliuyin <luoyuliuyin@gmail.com>
2019-04-09 10:35:31 +0800

    Update one-key-vps-centos7.sh

--- one-key-vps-centos7.sh
+++ one-key-vps-centos7.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #####---shadowsocks---#####;
-yum update -y;yum install epel-release -y;yum install vim wget net-tools python-setuptools -y;easy_install pip;pip install shadowsocks;echo 'alias sss="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '[Unit]\nDescription=Shadowsocks\n\n[Service]\nType=forking\nExecStart=/usr/bin/ssserver -p 443 -k dafeige -m rc4-md5 --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocks.service;systemctl enable shadowsocks;
+yum update -y;yum install epel-release -y;wget ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/Packages/l/libsodium-1.0.17-1.el7.x86_64.rpm;yum install vim wget net-tools python-setuptools libsodium-1.0.17-1.el7.x86_64.rpm -y;easy_install pip;pip install shadowsocks;echo 'alias sss="tail -f /var/log/shadowsocks.log"' >> /etc/bashrc;echo -e '[Unit]\nDescription=Shadowsocks\n\n[Service]\nType=forking\nExecStart=/usr/bin/ssserver -p 4399 -k pengfeixiong -m chacha20 --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocks.service;systemctl enable shadowsocks;
 
 #####---net_speeder---#####;
 #yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '[Unit]\nDescription=net_speeder\nAfter=network.service\n\n[Service]\nType=simple\nExecStart=/opt/net_speeder/net_speeder eth0 "ip"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/net_speeder.service;systemctl enable net_speeder;
