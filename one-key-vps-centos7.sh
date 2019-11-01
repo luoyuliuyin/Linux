5bf367b539d33afa30d17d3d970616e5b72c92e6
luoyuliuyin <luoyuliuyin@gmail.com>
2019-11-01 20:26:09 +0800

    Update one-key-vps-centos7.sh

--- one-key-vps-centos7.sh
+++ one-key-vps-centos7.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
 #####---shadowsocks---#####;
-yum update -y;yum install vim wget git -y;wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm;yum install libsodium-1.0.18-1.el7.x86_64.rpm -y;cd /opt;git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git;cd /opt/shadowsocksr;bash initcfg.sh;echo 'alias log="tail -f /var/log/shadowsocksr.log"' >> /etc/bashrc;echo -e '[Unit]\nDescription=Shadowsocksr\n\n[Service]\nType=forking\nExecStart=/usr/bin/python /opt/shadowsocksr/shadowsocks/server.py -p 4399 -k pengfeixiong -m chacha20 -O auth_aes128_md5 -o tls1.2_ticket_auth --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocksr.service;systemctl enable shadowsocksr;
+yum update -y;
+yum install vim wget git -y;
+wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm;
+yum install libsodium-1.0.18-1.el7.x86_64.rpm -y;
+cd /opt;
+git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git;
+cd /opt/shadowsocksr;
+bash initcfg.sh;
+echo 'alias log="tail -f /var/log/shadowsocksr.log"' >> /etc/bashrc;
+echo -e '[Unit]\nDescription=Shadowsocksr\n\n[Service]\nType=forking\nExecStart=/usr/bin/python /opt/shadowsocksr/shadowsocks/server.py -p 4399 -k pengfeixiong -m chacha20 -O auth_aes128_md5 -o tls1.2_ticket_auth --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocksr.service;
+systemctl enable shadowsocksr;
 
 #####---net_speeder---#####;
-yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '[Unit]\nDescription=net_speeder\nAfter=network.service\n\n[Service]\nType=simple\nExecStart=/opt/net_speeder/net_speeder eth0 "ip"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/net_speeder.service;systemctl enable net_speeder;
+#yum install libnet libnet-devel libpcap-devel gcc -y;wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/net-speeder/net_speeder-v0.1.tar.gz -O -|tar -xzv -C /opt;cd /opt/net_speeder;sh build.sh -DCOOKED;cd;echo -e '[Unit]\nDescription=net_speeder\nAfter=network.service\n\n[Service]\nType=simple\nExecStart=/opt/net_speeder/net_speeder eth0 "ip"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/net_speeder.service;systemctl enable net_speeder;
 
 #####—--SimpleHTTPServer---#####;
-echo -e '[Unit]\nDescription=SimpleHTTPServer\n\n[Service]\nType=forking\nWorkingDirectory=/root\nRestart=always\nRestartSec=1\nExecStart=/usr/bin/python -m SimpleHTTPServer 80\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/SimpleHTTPServer.service;systemctl enable SimpleHTTPServer;
+#echo -e '[Unit]\nDescription=SimpleHTTPServer\n\n[Service]\nType=forking\nWorkingDirectory=/root\nRestart=always\nRestartSec=1\nExecStart=/usr/bin/python -m SimpleHTTPServer 80\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/SimpleHTTPServer.service;systemctl enable SimpleHTTPServer;
+
+#####—--spring-boot-tools---#####;
+cd;
+wget https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.zip;
+unzip apache-maven-3.6.2-bin.zip;
+mv apache-maven-3.6.2 /opt;
+wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.rpm;
+yum install jdk-8u212-linux-x64.rpm -y;
+echo -e '# .bash_profile\n\n# Get the aliases and functions\nif [ -f ~/.bashrc ]; then\n        . ~/.bashrc\nfi\n\n# User specific environment and startup programs\nexport JAVA_HOME=/usr/java/default/\nexport MAVEN_HOME=/opt/apache-maven-3.6.2/\nPATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$MAVEN_HOME/bin\n\nexport PATH' > /root/.bash_profile;
+source /root/.bash_profile;
+cd /opt;
+git clone https://github.com/luoyuliuyin/spring-boot-tools.git;
+cd spring-boot-tools;
+mvn clean install;
+echo -e '[Unit]\n	Description=spring-boot-tools\n\n[Service]\n	Type=forking\n	WorkingDirectory=/root\n	Restart=always\n	RestartSec=1\n	ExecStart=/usr/bin/java -jar /opt/spring-boot-tools/target/spring-boot-tools.jar\n\n[Install]\n	WantedBy=multi-user.target' > /etc/systemd/system/spring-boot-tools.service;systemctl enable spring-boot-tools;
 
 #####—--ttf---#####;
 #wget -P /usr/share/fonts/yahei https://raw.githubusercontent.com/yakumioto/YaHei-Consolas-Hybrid-1.12/master/YaHei%20Consolas%20Hybrid%201.12.ttf;
@@ -15,4 +40,6 @@ echo -e '[Unit]\nDescription=SimpleHTTPServer\n\n[Service]\nType=forking\nWorkin
 #####---vnc---#####;
 #yum install gnome-classic-session gnome-terminal gnome-system-monitor tigervnc-server gedit expect firefox -y;sed -i 's/1024x768/1536x960/g' /usr/bin/vncserver;echo -e '[Unit]\nDescription=VNC\n\n[Service]\nType=simple\nExecStart=/usr/sbin/runuser -l root -c "/usr/bin/vncserver %i"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/vncserver@:1.service;systemctl enable vncserver@:1.service;vncpasswd;
 
-cd;rm -f *;reboot;
+cd;
+rm -f *;
+reboot;
