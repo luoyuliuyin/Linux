e4c5f6da87799fe1d0f71571b0d720821e98867a
luoyuliuyin <luoyuliuyin@gmail.com>
2019-11-22 11:12:26 +0800

    Update one-key-vps-centos8.sh

--- one-key-vps-centos8.sh
+++ one-key-vps-centos8.sh
@@ -2,7 +2,7 @@
 
 #####---shadowsocks---#####;
 yum update -y;
-yum install vim wget unzip git -y;
+yum install vim wget unzip git python36 -y;
 wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm;
 yum install libsodium-1.0.18-1.el7.x86_64.rpm -y;
 cd /opt;
@@ -10,7 +10,7 @@ git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git;
 cd /opt/shadowsocksr;
 bash initcfg.sh;
 echo 'alias log="tail -f /var/log/shadowsocksr.log"' >> /etc/bashrc;
-echo -e '[Unit]\nDescription=Shadowsocksr\n\n[Service]\nType=forking\nExecStart=/usr/bin/python /opt/shadowsocksr/shadowsocks/server.py -p 4399 -k pengfeixiong -m chacha20 -O auth_aes128_md5 -o tls1.2_ticket_auth --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocksr.service;
+echo -e '[Unit]\nDescription=Shadowsocksr\n\n[Service]\nType=forking\nExecStart=/usr/bin/python3 /opt/shadowsocksr/shadowsocks/server.py -p 4399 -k pengfeixiong -m chacha20 -O auth_aes128_md5 -o tls1.2_ticket_auth --workers 2 -d start\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/shadowsocksr.service;
 systemctl enable shadowsocksr;
 
 #####---net_speeder---#####;
@@ -36,7 +36,7 @@ systemctl enable spring-boot-tools;
 #wget -P /usr/share/fonts/yahei https://raw.githubusercontent.com/yakumioto/YaHei-Consolas-Hybrid-1.12/master/YaHei%20Consolas%20Hybrid%201.12.ttf;
 
 #####---vnc---#####;
-#yum install gnome-classic-session gnome-terminal gnome-system-monitor tigervnc-server gedit expect firefox -y;sed -i 's/1024x768/1536x960/g' /usr/bin/vncserver;echo -e '[Unit]\nDescription=VNC\n\n[Service]\nType=simple\nExecStart=/usr/sbin/runuser -l root -c "/usr/bin/vncserver %i"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/vncserver.service;systemctl enable vncserver.service;vncpasswd;
+#yum install gnome-classic-session gnome-terminal gnome-system-monitor tigervnc-server gedit firefox -y;sed -i 's/1024x768/1536x960/g' /usr/bin/vncserver;echo -e '[Unit]\nDescription=VNC\n\n[Service]\nType=simple\nExecStart=/usr/sbin/runuser -l root -c "/usr/bin/vncserver %i"\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/vncserver.service;systemctl enable vncserver.service;vncpasswd;
 
 cd;
 rm -f *;
