4bc68ed6a52c4325e7f4525bb160574b317c9e06
luoyuliuyin <luoyuliuyin@gmail.com>
2020-01-19 11:21:55 +0800

    Update one-key-vps-centos8.sh

--- one-key-vps-centos8.sh
+++ one-key-vps-centos8.sh
@@ -3,8 +3,8 @@
 #####---shadowsocksr---#####;
 yum update -y;
 yum install vim wget unzip git python36 -y;
-wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm;
-yum install libsodium-1.0.18-1.el7.x86_64.rpm -y;
+yum install epel-release -y;
+yum install libsodium -y;
 cd /opt;
 git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git;
 cd /opt/shadowsocksr;
