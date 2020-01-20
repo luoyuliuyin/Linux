451aed5c7af7740968ad51efc74c6b252635d940
luoyuliuyin <luoyuliuyin@gmail.com>
2020-01-20 10:20:13 +0800

    Update one-key-vps-centos8.sh

--- one-key-vps-centos8.sh
+++ one-key-vps-centos8.sh
@@ -43,6 +43,8 @@ systemctl enable spring-boot-tools;
 
 #####---reboot---#####;
 timedatectl set-timezone Asia/Shanghai;
+echo '0 2 * * * root yum upgrade -y' >> /etc/crontab;
+echo '0 3 * * * root bash <(curl -L -s https://install.direct/go.sh)' >> /etc/crontab;
 echo '0 4 * * * root reboot' >> /etc/crontab;
 
 cd;
